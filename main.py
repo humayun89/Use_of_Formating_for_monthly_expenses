04f6b5c31826394954c8c12d6c5a37d3f7fd9af5
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-10-24 20:04:22 +0300

    New commit

--- main.py
+++ main.py
@@ -3,11 +3,8 @@
 txt = "For only {price:.2f} dollars!"
 print(txt.format(price = 49))
 # Home work:
-bro="Avi´s monthly salary is {salary_mth:.2f},medical bill {medical_costs:.2f},bus bill {bus:.2f} euro !"
-salary_mth=input("Enter salary : ")
-medical_costs=input("Enter medical costs : ")
-bus = input("Enter monthly bus bill : ")
-print(bro.format(salary_mth, medical_costs, bus))
+bro="Avi's monthly salary is {salary_mth:.2f},medical bill {medical_costs:.2f},bus bill {bus:.2f} euro !"
+print(bro.format(salary_mth=float(input("Enter salary : ")), medical_costs=float(input("Enter medical costs : ")), bus = float(input("Enter monthly bus bill : "))))
 
 
 
