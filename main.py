d2d76a66fb860f48ca26e1f47e2560211ffa93d9
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-10-24 20:08:32 +0300

    New commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Use of format for different kind of problem to get a solution:
 #W3school pexample:
 txt = "For only {price:.2f} dollars!"
-print(txt.format(price = 49))
+print(txt.format(price=float(input("Enter the price : "))))
 # Home work:
 bro="Avi's monthly salary is {salary_mth:.2f},medical bill {medical_costs:.2f},bus bill {bus:.2f} euro !"
 print(bro.format(salary_mth=float(input("Enter salary : ")), medical_costs=float(input("Enter medical costs : ")), bus = float(input("Enter monthly bus bill : "))))
